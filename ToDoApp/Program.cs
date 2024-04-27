15caeae07c7e5781691201596f6bdebf056072cc
Javad Zanganeh <javad.zngh@gmail.com>
2024-04-27 21:16:48 +0330

    Daily

--- ToDoApp/Program.cs
+++ ToDoApp/Program.cs
@@ -1,10 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine();
-Console.WriteLine();
-//Hello commit
-Console.WriteLine();
+﻿Console.WriteLine("[S]ee all todo");
+Console.WriteLine("[A]dd a todo");
+Console.WriteLine("[R]emove a todo");
+Console.WriteLine("[E]xite todo app");
+
+int AliYearsOld = 29;
+int HosseinYearsOld = 29 ;
+if (AliYearsOld > HosseinYearsOld)
+    Console.WriteLine("Ali is Grater");
+else if ( HosseinYearsOld > AliYearsOld)
+    Console.WriteLine("Hossein is grater");
+else
+    Console.WriteLine("Equal");
+Console.ReadLine();
 //
-Console.WriteLine();
-//Git Fetch
-//Git Fetch 2
