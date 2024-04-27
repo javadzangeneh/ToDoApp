4e963c4806254146187d459f2b76e07f73baaa8e
Javad Zanganeh <javad.zngh@gmail.com>
2024-04-28 01:27:34 +0330

    Ex One.

--- ToDoApp/Program.cs
+++ ToDoApp/Program.cs
@@ -1,8 +1,8 @@
-﻿Console.WriteLine("[S]ee all todo");
-Console.WriteLine("[A]dd a todo");
-Console.WriteLine("[R]emove a todo");
-Console.WriteLine("[E]xite todo app");
-
+﻿//Console.WriteLine("[S]ee all todo");
+//Console.WriteLine("[A]dd a todo");
+//Console.WriteLine("[R]emove a todo");
+//Console.WriteLine("[E]xite todo app");
+/*
 int AliYearsOld = 29;
 int HosseinYearsOld = 29 ;
 if (AliYearsOld > HosseinYearsOld)
@@ -10,5 +10,26 @@ if (AliYearsOld > HosseinYearsOld)
 else if ( HosseinYearsOld > AliYearsOld)
     Console.WriteLine("Hossein is grater");
 else
-    Console.WriteLine("Equal");
+    Console.WriteLine("Equal");//
 Console.ReadLine();
+*/
+Console.WriteLine("Please Inter Number One: ");
+var numA= Console.ReadLine();
+Console.WriteLine("Please Inter Number Two: ");
+var numB = Console.ReadLine();
+Console.WriteLine("Please Inter A,a Or + For Sum");
+Console.WriteLine("Please Inter S,s Or - For sub");
+Console.WriteLine("Please Inter M,m Or * for Zarb");
+var @operator = Console.ReadLine();
+ 
+Action(int.Parse(numA), int.Parse(numB), @operator.ToString());
+
+void Action (int a=1 , int b = 1, string op="+")
+{
+    if (op == "A" || op == "a" || op == "+")
+        Console.WriteLine(a + b + " " + op);
+    if (op == "S" || op == "s" || op == "-")
+        Console.WriteLine(a - b + " " + op);
+    if (op == "M"||  op == "m" || op=="*")
+        Console.WriteLine(a * b + " " + op);
+}
